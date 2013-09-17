329b704010ee639312614d96cef368331288f1e8
ltrainpr <apprentice@dbc15.local>
2013-09-17 16:43:29 -0700

    Build Topic Display Page

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require_relative '../application'
 
 get '/' do
-  @topics = Topic.all#.order('updated_at')
+  @topics = Topic.all.order('updated_at DESC')
   erb :home
 end
 
@@ -16,5 +16,11 @@ post '/new_topic' do
   t.title = params[:title]
   t.body = params[:body]
   t.save
+  redirect '/'
+end
+
+get '/:id' do
+  @topic = Topic.find(params[:id])
+  erb :topic
 end
 
