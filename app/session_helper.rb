4a547d0a12a23fc927009861abf2b946431ee6e2
ltrainpr <apprentice@dbc15.local>
2013-09-18 10:24:30 -0700

    add user login and authentication helpers

--- app/session_helper.rb
+++ app/session_helper.rb
@@ -1,3 +1,19 @@
 helpers do
 
+  def find_user
+    @user = User.find_by_email(params[:email])
+  end
+
+  def authenticate
+     give_token if @user.password == params[:password]
+  end
+
+  def give_token
+    session[:user_id] = @user.id
+  end
+
+  def login
+    authenticate unless find_user.nil?
+  end
+
 end
