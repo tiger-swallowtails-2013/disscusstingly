1c40bbb2c57ef7a7c19e588947eb36cc6fdd9f69
nathanallen <thenathanator@gmail.com>
2013-09-18 09:39:59 -0700

    add Topic and Comment tests

--- spec/shoulda_spec.rb
+++ spec/shoulda_spec.rb
@@ -2,9 +2,22 @@ require 'rspec'
 require 'shoulda'
 require_relative '../application'
 
+
 describe User do
   it { should validate_presence_of(:password)}
   it { should validate_uniqueness_of(:email)}
   it { should have_many(:topics) }
   it { should have_many(:comments) }
+end
+
+describe Topic do
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:body)}
+  it { should have_one(:user) }
+end
+
+describe Comment do
+  it { should validate_presence_of(:body)}
+  it { should have_one(:user) }
+  it { should have_one(:topic) }
 end
