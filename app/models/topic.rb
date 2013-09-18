23a73f35363fb3869ce635c24b62d80922335e66
nathanallen <nathan@server.fake>
2013-09-17 23:18:50 -0700

    add user alias 'author' to comment and topic models

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 require_relative '../../application'
 class Topic < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :comments
   validates_presence_of :title, :body
 
