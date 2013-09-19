c35e02fa10bb84970bfc671bd0b45124fda96a82
nathanallen <thenathanator@gmail.com>
2013-09-18 18:57:03 -0700

    remove sqlite:// from DB_PATH

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,9 @@ end
 
 adapter = 'sqlite3'
 if settings.test?
-  DB_PATH = "sqlite3:///db/Disscusstingly_test.db"
+  DB_PATH = "../db/Disscusstingly_test.db"
 elsif settings.development?
-  DB_PATH = "sqlite3:///db/Disscusstingly_development.db"
+  DB_PATH = "../db/Disscusstingly_development.db"
 else
   DB_PATH = ENV['DATABASE_URL']
   adapter = 'postgresql'
