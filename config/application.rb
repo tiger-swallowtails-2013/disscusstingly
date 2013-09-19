e6ae16e746a9e59275396ac63a02d85026f4ba3b
Lionel Ramos <lionelramos@Lionels-MacBook-Pro.local>
2013-09-19 16:40:04 -0700

    Changing Active Record Postgres connection

--- config/application.rb
+++ config/application.rb
@@ -24,17 +24,9 @@ end
 #                                         :database => DB_PATH
 # else
 #   configure :production do
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
  
-	ActiveRecord::Base.establish_connection(
-			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-			:host     => db.host,
-			:username => db.user,
-			:password => db.password,
-			:database => db.path[1..-1],
-			:encoding => 'utf8',
-			:port => db.port
-	)
-	# end
-# end  
+	ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+
 
