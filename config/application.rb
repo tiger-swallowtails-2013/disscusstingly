fc3ae795b88972601bb46c6e623860fb18f2bd66
Lionel Ramos <lionelramos@Lionels-MacBook-Pro.local>
2013-09-19 17:47:29 -0700

    asdf

--- config/application.rb
+++ config/application.rb
@@ -3,26 +3,26 @@ require 'pathname'
 require 'active_record'
 require 'sqlite3' unless settings.production? 
 
-APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
+# APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
 
-APP_NAME = APP_ROOT.basename.to_s
+# APP_NAME = APP_ROOT.basename.to_s
 
-set :root, APP_ROOT.join("app")
+# set :root, APP_ROOT.join("app")
 
-Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
-  filename = File.basename(model_file).gsub('.rb', '')
-  autoload ActiveSupport::Inflector.camelize(filename), model_file
-end
+# Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+#   filename = File.basename(model_file).gsub('.rb', '')
+#   autoload ActiveSupport::Inflector.camelize(filename), model_file
+# end
 
-adapter = 'sqlite3'
-if settings.test?
-  DB_PATH = "#{APP_ROOT}/db/Disscusstingly_test.db"
-elsif settings.development?
-  DB_PATH = "#{APP_ROOT}/db/Disscusstingly_development.db"
-else
+# adapter = 'sqlite3'
+# if settings.test?
+#   DB_PATH = "#{APP_ROOT}/db/Disscusstingly_test.db"
+# elsif settings.development?
+#   DB_PATH = "#{APP_ROOT}/db/Disscusstingly_development.db"
+# else
   DB_PATH = ENV['DATABASE_URL']
   adapter = 'postgresql'
-end  
+# end  
 
 ActiveRecord::Base.establish_connection :adapter  => adapter,
                                         :database => DB_PATH
